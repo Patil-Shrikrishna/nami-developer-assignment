eea0cf75cc641005071448737f23a500972fa336
Patil-Shrikrishna <krishnapt1993@gmail.com>
2024-04-06 18:16:36 +0530

    [add]: Required components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import one from "./assets/one.png";
+import swap from "./assets/swap.png";
+import priority from "./assets/priority.png";
+import clock from "./assets/clock.png";
+import StatusCard from "./components/StatusCard";
 // import SideNav from "./components/SideNav";
 // import MobileFooter from "./components/MobileFooter";
 // import TopNav from "./components/TopNav";
@@ -6,12 +11,87 @@ import React from "react";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="">
       {/* <h1 className="text-5xl font-bold underline">Hello world!</h1> */}
       {/* <MobileHeader /> */}
       {/* <TopNav /> */}
       {/* <MobileFooter /> */}
       {/* <SideNav /> */}
+
+      <StatusCard
+        bgColor="bg-[#CC2610]"
+        textColor="text-[#FFFFFF]"
+        status="Pending"
+      />
+      <StatusCard
+        bgColor="bg-[#F1AE11]"
+        textColor="text-[#FFFFFF]"
+        status="Ongoing"
+      />
+      <StatusCard
+        bgColor="bg-[#00A441]"
+        textColor="text-[#FFFFFF]"
+        status="On-Time"
+      />
+      <StatusCard
+        bgColor="bg-[#CC2610]"
+        textColor="text-[#FFFFFF]"
+        status="Priority"
+        icon={priority}
+      />
+      <StatusCard bgColor="bg-[#00A441]" icon={clock} />
+      <StatusCard bgColor="bg-[#CC2610]" icon={clock} />
+
+      <StatusCard
+        bgColor="bg-[#F4EBD8]"
+        textColor="text-[#F1AE11]"
+        status="Pending"
+      />
+      <StatusCard
+        bgColor="bg-[#D7FFE7]"
+        textColor="text-[#00A441]"
+        status="Resolved"
+      />
+      <StatusCard
+        bgColor="bg-[#F4D8D8]"
+        textColor="text-[#CC2610]"
+        status="Rejected"
+      />
+      <StatusCard
+        bgColor="bg-[#F7F7F7]"
+        textColor="text-[#5F69C7]"
+        status="Internal Task"
+      />
+      <StatusCard
+        bgColor="bg-[#F7F7F7]"
+        textColor="text-[#DC5B19]"
+        status="Guest Task"
+      />
+      <StatusCard
+        bgColor="bg-[#F7F7F7]"
+        textColor="text-[#CC2610]"
+        status="Not Accepted"
+      />
+      <StatusCard
+        bgColor="bg-[#F7F7F7]"
+        textColor="text-[#F1AE11]"
+        status="Ongoing"
+      />
+      <StatusCard
+        bgColor="bg-[#F7F7F7]"
+        textColor="text-[#1A0A02]"
+        status="Scheduled"
+      />
+      <StatusCard
+        bgColor="bg-[#F7F7F7]"
+        textColor="text-[#1A0A02]"
+        icon={swap}
+      />
+      <StatusCard
+        bgColor="bg-[#F7F7F7]"
+        textColor="text-[#1A0A02]"
+        icon={one}
+      />
     </div>
   );
 };
---
+++ src/components/StatusCard.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const StatusCard: React.FC<{
+  status?: string;
+  icon?: string;
+  bgColor?: string;
+  textColor?: string;
+}> = ({ status, icon, bgColor, textColor }) => {
+  return (
+    <div
+      className={`${bgColor} ${textColor} ${
+        status && icon ? "gap-2" : ""
+      } p-2 flex justify-center items-center text-center w-fit rounded-full`}
+    >
+      <p>{status}</p>
+      {icon && <img src={icon} className="w-4" />}
+    </div>
+  );
+};
+
+export default StatusCard;
